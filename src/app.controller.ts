28b65be7cc178a4929ebd3a67abc803abe2bb7e1
DANG-PH <dangph.ptit@gmail.com>
2026-03-19 14:42:34 +0700

    up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('health')
+  async healthCheck() {
+    return {
+      success: true
+    };
+  }
 }
